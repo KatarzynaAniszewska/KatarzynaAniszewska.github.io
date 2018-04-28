4c06ddb26db0f4ef668c750836b6ae1aa8ebd808
KatarzynaAniszewska <33962159+KatarzynaAniszewska@users.noreply.github.com>
2018-04-28 08:38:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://protected-anchorage-34068.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://protected-anchorage-34068.herokuapp.com/v1/trello';
+  const trelloApiRoot = 'https://protected-anchorage-34068.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
